27e8c4b178fea080dd49dcbbc45038f41c0943b2
Matt Garrett <magarrett11@gmail.com>
2018-09-30 10:33:20 -0600

    revised newspaper hash

--- newspaper.js
+++ newspaper.js
@@ -1,85 +1,92 @@
 function hash(string, max){
-    var hash =0;
-    for (i=0; i<string.length; i++){
-        hash +=string.charCodeAt(i);
-    }
-    return hash % max;
+  var hash =0;
+  for (i=0; i<string.length; i++){
+      hash +=string.charCodeAt(i);
+  }
+  return hash % max;
 };
 
 let Paper = function() {
-  
-    let newspapers = [];
-    const storageLimit = 100;
-    
-    this.print = function() {
-      console.log(newspapers);
-    }
-    
-      this.add = function(key, value) {
-        var index = hash(key, storageLimit);
-        if(newspapers[index] === undefined) {
-          newspapers[index] = [
-            [key, value]
-          ];
-        }else {
-          var inserted = false;
-          for (var i = 0; i < newspapers[index].length; i++) {
-            if(newspapers[index][i][0] === key) {
-              newspapers[index][i][1] = value;
-              inserted = true;
-            }
-          }
-          if (inserted === false) {
-            newspapers[index].push([key, value]);
-          }
-        }
-    };
 
-    this.remove = function(key){
-        var index = hash(key, storageLimit);
-        if(newspapers[index].length === 1 && newspapers[index][0][0]===key){
-            delete newspapers[index];
-        } else {
-            for (var i =0; i < newspapers[index]; i++){
-                if (newspapers[index][i][0]=== key){
-                    delete newspapers[index][i];
-                }
-            }
-        }
-    };
+  let newspapers = [];
+  const storageLimit = 100;
   
-    this.lookup = function(key) {
-      var index = hash(key, storageLimit)
-      if (newspapers[index] === undefined) {
-        return undefined;
-      } else {
+  this.print = function() {
+    console.log(newspapers);
+  }
+  
+    this.add = function(key, value) {
+      var index = hash(key, storageLimit);
+      if(newspapers[index] === undefined) {
+        newspapers[index] = [
+          [key, value]
+        ];
+      }else {
+        var inserted = false;
         for (var i = 0; i < newspapers[index].length; i++) {
-            for (var j = 0; j < newspapers[index].length; j++) {
-                if(newspapers[index][i][0] === key && newspapers[index][j][1]===value) {
-                    return newspapers[index][i][1];
-            }
+          if(newspapers[index][i][0] === key) {
+            newspapers[index][i][1] = value;
+            inserted = true;
           }
+        }
+        if (inserted === false) {
+          newspapers[index].push([key, value]);
         }
       }
-    };
   };
-  
-  
 
- let ht = new Paper();
- 
-  function addPaper() {
-    let addCustBool = true;
-      let puslisher = prompt("Enter publisher name: ");
-      let pubDate = prompt("Enter publication date in DDMMYYY format: ");
-      ht.add(publisher, pubDate);
-        
+  this.remove = function(key){
+      var index = hash(key, storageLimit);
+      if(newspapers[index].length === 1 && newspapers[index][0][0]===key){
+          delete newspapers[index];
+      } else {
+          for (var i =0; i < newspapers[index]; i++){
+              if (newspapers[index][i][0]=== key){
+                  delete newspapers[index][i];
+              }
+          }
+      }
+  };
+
+  this.lookup = function(key,value) {
+    var index = hash(key, storageLimit)
+    for (var i = 0; i < newspapers[index].length; i++) {
+        if(newspapers[index][i][0] === key && newspapers[index][i][1]===value) {
+           return 'publication date: ' + newspapers[index][i][1] + ', publisher: ' + newspapers[index][i][0];
+    } else {
+      return undefined;
       
-    ht.print();
+      }
+    }
   };
+};
 
-  function findPaper() {
-    let paperSearch = prompt("Search by publisher name:");
-    let dateSearch = prompt("Search by publication date in DDMMYYY format:")
-    console.log(ht.lookup(paperSearch, dateSearch));
-  }
+// prompt function to add paper call with addPaper();
+function addPaper() {
+  let addCustBool = true;
+    let publisher = prompt("Enter publisher name: ");
+    let pubDate = prompt("Enter publication date in DDMMYYY format: ");
+    ht.add(publisher, pubDate);
+      
+    
+  ht.print();
+};
+// prompt function to lookup paper call with findPaper();
+function findPaper() {
+  let paperSearch = prompt("Search by publisher name:");
+  let dateSearch = prompt("Search by publication date in DDMMYYY format:")
+  console.log(ht.lookup(paperSearch, dateSearch));
+}
+
+
+
+let ht = new Paper();
+
+
+ht.add('matt','12121980')
+ht.add('abby', '30303030')
+console.log(ht.lookup('matt','12121980'));
+
+
+
+
